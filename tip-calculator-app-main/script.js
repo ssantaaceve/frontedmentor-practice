5f64704b1cdc9a8a3283ec6ae6cdf181e65c3fb0
Sergio Santa <sergiosanta970617@gmail.com>
2022-10-16 22:22:57 -0500

    Terminada la primer visual, pendiente el JS

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,61 @@
+const dinero = document.getElementById('ValorGanancia');
+const resultado = document.getElementsByClassName('Reset');
+
+resultado.addEventListener('onclick', console.log(dinero));
+
+
+// function Calculodetasa(){
+//     alert ('hola mundo');
+// }
+
+
+
+
+
+// <section class="Section">
+//     <menu class="Section-Calculadora">
+//       <div class="Section-Calculadora_NomCalculadora">
+//         <h1>SPLITTER</h1>
+//       </div>
+//       <div class="Section-Calculadora_CalculoCalculadora">
+//         <div class="CalculoCalculadora-IngresoTotal">
+//           <p>Bill </p>
+//           <input type="number" name="ValorGanancia" id="ValorGanancia" placeholder="0">
+//           <span class="Logopesos"> <img class="imgPesos" src="./images/icons8-dólar-estadounidense-24.png" alt="" srcset=""></span>
+//         </div>
+//         <div class="CalculoCalculadora-tipCalcu">
+//           <p>Select Tip %</p>
+//           <div class="CalculoCalculadora-tipCalculadora">
+//             <div class="%tipCalculadora5%">5</div>
+//             <div class="%tipCalculadora10%">10</div>
+//             <div class="%tipCalculadora15%">15</div>
+//             <div class="%tipCalculadora25%">25</div>
+//             <div class="%tipCalculadora50%">50</div>
+//             <div class="%tipCalculadoraCustom">Custom</div>
+//           </div>
+//         </div>
+//         <div class="CalculoCalculadora-NumDePersonas">
+//           <p>Number of People</p>
+//           <input type="number" name="ValorGanancia" id="ValorGanancia" placeholder="0">
+//           <span class="LogoPerson"><img class="SpanUser" src="./images/icons8-user-30.png" alt=""></span>
+//         </div>
+//         <div class="CalculoCalculadora-Resultado">
+//           <div class="Resultado_TipAmount">
+//             <div class="text-TipAmount">
+//               <p class="text-resulta-p1">TipAmount</p>
+//               <p class="text-resulta-p2">/Person</p>
+//             </div>
+//             <p class="Valor-resulta-p3">$0.0</p>
+//           </div>
+//           <div class="Resultado_Total">
+//             <div class="text-Total">
+//               <p class="text-resulta-p1">Total</p>
+//               <p class="text-resulta-p2">/Person</p>
+//             </div>
+//             <p class="Valor-resulta-p3">$0.0</p>
+//           </div>
+//           <button>RESET</button>
+//         </div>
+//       </div>
+//     </menu>
+//   </section>
